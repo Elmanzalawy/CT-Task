ccb594fbb1adfc9341207cb2fdae29fe72e8dffb
Mohamed Elmanzalawy <mohamed.elmanzalawy98@gmail.com>
2022-04-01 21:42:23 +0200

    Add Project CRUD functions

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Project::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $project = Project::create([
+            'name' => $request->name,
+        ]);
+
+        return $project->fresh();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Project $project)
+    {
+        return $project;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Project $project)
+    {
+        $project->update([
+            'name' => $request->name ?? $project->name,
+        ]);
+
+        return $project;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Project $project)
+    {
+        return $project->delete();
+    }
+}
