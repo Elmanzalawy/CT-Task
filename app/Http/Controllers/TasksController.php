d16afcf79bb29106bd84cf934a55e2c0a87e0cfd
Mohamed Elmanzalawy <mohamed.elmanzalawy98@gmail.com>
2022-04-01 21:43:08 +0200

    Add Task CRUD functions

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TasksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        return Task::when($request->project_id, function($task) use ($request){
+            $task->where("project_id", $request->project_id);
+        })
+        ->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $task = Task::create([
+            'name' => $request->name,
+            'priority' => $request->priority,
+            'project_id' => $request->project_id ?? null,
+        ]);
+
+        return $task->fresh();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Task $task)
+    {
+        return $task;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Task $task)
+    {
+        $task->update([
+            'name' => $request->name ?? $task->name,
+            'priority' => $request->priority ?? $task->priority,
+        ]);
+
+        return $task;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Task $task)
+    {
+        return $task->delete();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProjectsController;
+use App\Http\Controllers\TasksController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('task', TasksController::class);
 Route::apiResource('project', ProjectsController::class);
